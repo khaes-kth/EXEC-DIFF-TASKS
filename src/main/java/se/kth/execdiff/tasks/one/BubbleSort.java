262c435444e2b2b418aa15bb50c802560b14b2eb
Khashayar Etemadi <khaes@kth.se>
2022-07-11 13:35:12 +0200

    bug fixed

--- src/main/java/se/kth/execdiff/tasks/one/BubbleSort.java
+++ src/main/java/se/kth/execdiff/tasks/one/BubbleSort.java
@@ -2,8 +2,8 @@ package se.kth.execdiff.tasks.one;
 
 public class BubbleSort {
     public static void sort(int[] arr){
-        for(int i = arr.length; i >= 0; i--){
-            for(int j = 0; j < i; j++){
+        for(int i = arr.length - 1; i >= 0; i--){
+            for(int j = 0; j < i - 1; j++){
                 if(arr[j + 1] < arr[j]){
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
