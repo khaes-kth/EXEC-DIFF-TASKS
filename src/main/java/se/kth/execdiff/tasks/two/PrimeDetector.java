d794e0b80b14429d9e374ca1a96c3ed100ef3c60
Khashayar Etemadi <khaes@kth.se>
2022-07-11 14:40:06 +0200

    task 2 patched added

--- src/main/java/se/kth/execdiff/tasks/two/PrimeDetector.java
+++ src/main/java/se/kth/execdiff/tasks/two/PrimeDetector.java
@@ -3,7 +3,7 @@ package se.kth.execdiff.tasks.two;
 public class PrimeDetector {
     public static int getSmallestPrimeFactor(int t){
         int smallestFactor = 1000000000;
-        for(int i = (int) Math.sqrt(t); i > 1; i--){
+        for(int i = 2; i < Math.sqrt(t); i++){
             if(t % i == 0){
                 smallestFactor = i;
             }
